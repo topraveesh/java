5763448efe8349c4d5873c4aa6c22b947f862674
topraveesh <topraveesh@gmail.com>
2014-11-10 16:26:49 +0530

    Create Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+
+public class Factorial {
+	
+	public static void main(String[] args) {
+		
+		Factorial f = new Factorial();
+		
+		System.out.println("Enter the number: "); 
+		
+		Scanner s = new Scanner(System.in);
+		int n =s.nextInt();
+		System.out.println(f.findFactNum(n));
+
+	}
+	
+	public int findFactNum(int num){
+		if (num >0)
+		return (num*findFactNum(num-1));
+		return 1;
+	}
+		  
+}
