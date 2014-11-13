2173e5f5e1d876ee07adeb08d524a06295f49ec0
topraveesh <topraveesh@gmail.com>
2014-11-13 12:38:55 +0530

    Create B.java

---
+++ AnonymousInnerClass/B.java
@@ -0,0 +1,17 @@
+package AnonymousInnerClass;
+
+public class B {
+
+	// in the below line, 'a' is actually pointing to the anonymous class Object
+	A a = new A(){
+		public void read()
+		{
+			System.out.println("Anonymous read()");
+		}
+	};
+	
+	public static void main(String[] args) {
+		B b = new B();
+		b.a.read(); 
+	}
+}
