19915ed6be53fb5412cc07437613389856306d88
topraveesh <topraveesh@gmail.com>
2014-11-25 14:23:36 +0530

    Create ByteStreamExample.java

---
+++ Streams_Files_IO/ByteStreamExample.java
@@ -0,0 +1,48 @@
+import java.io.*;
+
+public class Test {
+    public static void main(String [] args) {
+
+        // The name of the file to open.
+        String fileName = "temp.txt";
+
+        try {
+            // Use this for reading the data.
+            byte[] buffer = new byte[1000];
+
+            FileInputStream inputStream = 
+                new FileInputStream(fileName);
+
+            // read fills buffer with data and returns
+            // the number of bytes read (which of course
+            // may be less than the buffer size, but
+            // it will never be more).
+            int total = 0;
+            int nRead = 0;
+            while((nRead = inputStream.read(buffer)) != -1) {
+                // Convert to String so we can display it.
+                // Of course you wouldn't want to do this with
+                // a 'real' binary file.
+                System.out.println(new String(buffer));
+                total += nRead;
+            }	
+
+            // Always close files.
+            inputStream.close();		
+
+            System.out.println("Read " + total + " bytes");
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println(
+                "Unable to open file '" + 
+                fileName + "'");				
+        }
+        catch(IOException ex) {
+            System.out.println(
+                "Error reading file '" 
+                + fileName + "'");					
+            // Or we could just do this: 
+            // ex.printStackTrace();
+        }
+    }
+}
